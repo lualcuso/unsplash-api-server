c77398b921a623e4f437ad6cce706a7b8393e335
Luis Cubillo <luis.cubillo.soto@gmail.com>
2023-01-31 23:50:45 -0600

    Adds search endpoint

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
+require('dotenv').config()
+
 const express = require('express')
+const https = require('https')
 const app = express()
 const port = 3001
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const unsplashAPIURL = 'api.unsplash.com';
+
+app.get('/search', function (req, res) {
+  
+  https.get(`https://${unsplashAPIURL}/search/photos?page=${req.query.page}&query=${req.query.search}`,
+    {headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Client-ID ${process.env.UNSPLASH_ACCESS_KEY}`
+    }
+  }, function (response) {
+    let searchData = '';
+    response.on('data', (chunk) => {
+      searchData += chunk;
+    });
+    response.on('end', () => {
+      res.json(JSON.parse(searchData));
+    });
+  })
 })
 
 app.listen(port, () => {
